2f0a26000efa5f141e331133d718faeb57a7315f
Florian Hartmann <jscoderr@gmail.com>
2018-06-19 13:48:48 -0700

    Fix synchronization spacing

--- synchronization.js
+++ synchronization.js
@@ -1,36 +1,36 @@
-ChromeUtils.import("resource://gre/modules/TelemetryController.jsm")
+ChromeUtils.import('resource://gre/modules/TelemetryController.jsm')
 
-URL_ENDPOINT = "https://s3-us-west-2.amazonaws.com/telemetry-test-bucket/frecency/latest.json"
+URL_ENDPOINT = 'https://s3-us-west-2.amazonaws.com/telemetry-test-bucket/frecency/latest.json'
 TIME_INTERVAL = 1 * 60 * 1000
-//TIME_INTERVAL = 30 * 60 * 1000
+// TIME_INTERVAL = 30 * 60 * 1000
 EXTRA_DELAY = 5 * 1000
-//EXTRA_DELAY = 30 * 1000
-TELEMETRY_TOPIC = "frecency_update"
+// EXTRA_DELAY = 30 * 1000
+TELEMETRY_TOPIC = 'frecency_update'
 
 class ModelSynchronization {
   constructor () {
-  	this.iteration = -1
-  	this.fetchModel()
-  	this.bindEvents()
+    this.iteration = -1
+    this.fetchModel()
+    this.bindEvents()
   }
 
   bindEvents () {
-	let now = new Date()
-	let firstDelay = TIME_INTERVAL - now.getSeconds() * 1000
-	firstDelay += EXTRA_DELAY
-	setTimeout(this.fetchModel.bind(this), firstDelay)
+    let now = new Date()
+    let firstDelay = TIME_INTERVAL - now.getSeconds() * 1000
+    firstDelay += EXTRA_DELAY
+    setTimeout(this.fetchModel.bind(this), firstDelay)
   }
 
   fetchModel () {
-  	fetch(URL_ENDPOINT)
-  	  .then((response) => response.json())
- 	  .then(this.applyModelUpdate.bind(this))
+    fetch(URL_ENDPOINT)
+      .then((response) => response.json())
+      .then(this.applyModelUpdate.bind(this))
 
-	setTimeout(this.fetchModel.bind(this), TIME_INTERVAL + EXTRA_DELAY)
+    setTimeout(this.fetchModel.bind(this), TIME_INTERVAL + EXTRA_DELAY)
   }
 
   applyModelUpdate ({ iteration, model }) {
-  	this.iteration = iteration
+    this.iteration = iteration
 
     for (let i = 0; i < PREFS.length; i++) {
       Services.prefs.setIntPref(PREFS[i], model[i])
@@ -38,16 +38,15 @@ class ModelSynchronization {
   }
 
   pushModelUpdate (weights) {
-  	let payload = {
-  	  iteration: this.iteration,
-	  weights
-  	}
+    let payload = {
+      iteration: this.iteration,
+      weights
+    }
 
-  	let options = {
-  	  addClientId: true
-  	}
+    let options = {
+      addClientId: true
+    }
 
-  	TelemetryController.submitExternalPing(TELEMETRY_TOPIC, payload, options)
+    TelemetryController.submitExternalPing(TELEMETRY_TOPIC, payload, options)
   }
 }
-
