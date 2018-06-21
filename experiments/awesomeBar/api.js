eb46681fa432ef4baa1997ad4a4ef194024df673
Florian Hartmann <jscoderr@gmail.com>
2018-06-21 12:55:07 -0700

    Add awesome bar API

--- experiments/awesomeBar/api.js
+++ experiments/awesomeBar/api.js
@@ -4,7 +4,15 @@
 
 ChromeUtils.import("resource://gre/modules/Services.jsm");
 
-/* https://firefox-source-docs.mozilla.org/toolkit/components/extensions/webextensions/functions.html */
+const { ExtensionCommon  } = ChromeUtils.import(
+   "resource://gre/modules/ExtensionCommon.jsm",
+);
+
+const { EventManager  } = ExtensionCommon;
+
+const EVENT = 'autocomplete-will-enter-text'
+const NON_HISTORY_STYLES = ['switchtab', 'remotetab', 'searchengine', 'visiturl', 'extension', 'suggestion', 'keyword'] // bookmark
+
 var awesomeBar = class extends ExtensionAPI {
   getAPI(context) {
     return {
@@ -15,11 +23,43 @@ var awesomeBar = class extends ExtensionAPI {
             context,
             name: "awesomeBar.onHistorySearch",
             register: fire => {
-              fire(1)
+              Services.obs.addObserver(el => processAwesomeBarSearch(el, fire.async), 'autocomplete-will-enter-text', false)
             }
           }).api()
         }
       }
     }
   }
+}
+
+function processAwesomeBarSearch(el, callback) {
+  let popup = el.popup
+  let controller = popup.view.QueryInterface(Ci.nsIAutoCompleteController)
+
+  let selectedIndex = popup.selectedIndex
+  let selectedStyle = controller.getStyleAt(selectedIndex)
+  let searchQuery = controller.searchString
+
+  if (isHistoryStyle(selectedStyle) && searchQuery !== '') {
+    let numberOfSuggestions = controller.matchCount
+    let historySuggestions = []
+
+    for (var i = 0; i < numberOfSuggestions; i++) {
+      let url = controller.getFinalCompleteValueAt(i)
+      let isHistory = isHistoryStyle(controller.getStyleAt(i))
+
+      if (isHistory) {
+        historySuggestions.push(url)
+      }
+    }
+
+    let selectedHistoryIndex = historySuggestions.indexOf(controller.getFinalCompleteValueAt(selectedIndex))
+    callback(historySuggestions, selectedHistoryIndex)
+  }
+}
+
+function isHistoryStyle(styleString) {
+  let styles = new Set(styleString.split(/\s+/))
+  let isNonHistoryStyle = NON_HISTORY_STYLES.some(s => styles.has(s))
+  return !isNonHistoryStyle
 }
