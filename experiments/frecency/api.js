c5dc8d55a7619f277fe0478e84d17bd51503bd5c
Florian Hartmann <jscoderr@gmail.com>
2018-07-03 10:46:00 -0700

    Perform all SQL calls async

--- experiments/frecency/api.js
+++ experiments/frecency/api.js
@@ -4,19 +4,19 @@ ChromeUtils.import('resource://gre/modules/PlacesUtils.jsm')
 
 const CHUNK_SIZE = 5000
 
-function removeFrecencyTrigger () {
-  let db = PlacesUtils.history.DBConnection
-  let stmt = db.createStatement('DROP TRIGGER IF EXISTS moz_places_afterupdate_frecency_trigger')
-  stmt.executeStep()
+async function removeFrecencyTrigger () {
+  return await PlacesUtils.withConnectionWrapper("federated-learning", async db => {
+    db.execute('DROP TRIGGER IF EXISTS moz_places_afterupdate_frecency_trigger')
+  })
 }
 
-function restoreFrecencyTrigger () {
+async function restoreFrecencyTrigger () {
   // Query from https://dxr.mozilla.org/mozilla-central/source/toolkit/components/places/nsPlacesTriggers.h#176
-  let db = PlacesUtils.history.DBConnection
-  let stmt = db.createStatement(`
+  return await PlacesUtils.withConnectionWrapper("federated-learning", async db => {
+    db.execute(`
 CREATE TEMP TRIGGER moz_places_afterupdate_frecency_trigger AFTER UPDATE OF frecency ON moz_places FOR EACH ROW WHEN NEW.frecency >= 0 AND NOT is_frecency_decaying() BEGIN UPDATE moz_origins SET frecency = ( SELECT IFNULL(MAX(frecency), 0) FROM moz_places WHERE moz_places.origin_id = moz_origins.id  ) WHERE id = NEW.origin_id; INSERT OR REPLACE INTO moz_meta(key, value) VALUES ( 'frecency_count', CAST(IFNULL((SELECT value FROM moz_meta WHERE key = 'frecency_count'), 0) AS INTEGER) - (CASE WHEN OLD.frecency <= 0 OR OLD.id < 0 THEN 0 ELSE 1 END) + (CASE WHEN NEW.frecency <= 0 OR NEW.id < 0 THEN 0 ELSE 1 END)  ), ( 'frecency_sum', CAST(IFNULL((SELECT value FROM moz_meta WHERE key = 'frecency_sum'), 0) AS INTEGER) - (CASE WHEN OLD.frecency <= 0 OR OLD.id < 0 THEN 0 ELSE OLD.frecency END) + (CASE WHEN NEW.frecency <= 0 OR NEW.id < 0 THEN 0 ELSE NEW.frecency END)  ), ( 'frecency_sum_of_squares', CAST(IFNULL((SELECT value FROM moz_meta WHERE key = 'frecency_sum_of_squares'), 0) AS INTEGER) - (CASE WHEN OLD.frecency <= 0 OR OLD.id < 0 THEN 0 ELSE OLD.frecency * OLD.frecency END) + (CASE WHEN NEW.frecency <= 0 OR NEW.id < 0 THEN 0 ELSE NEW.frecency * NEW.frecency END)  ); END
-`)
-  stmt.executeStep()
+    `)
+  })
 }
 
 var frecency = class extends ExtensionAPI {
@@ -32,7 +32,7 @@ var frecency = class extends ExtensionAPI {
           },
 
           async updateAllFrecencies () {
-            removeFrecencyTrigger()
+            await removeFrecencyTrigger()
 
             let res = await PlacesUtils.withConnectionWrapper('federated-learning', async db => db.execute('SELECT COUNT(*) as count FROM moz_places'))
             let count = res[0].getResultByName('count')
@@ -47,7 +47,7 @@ var frecency = class extends ExtensionAPI {
               })
             }
 
-            return Promise.all(promises).then(restoreFrecencyTrigger)
+            return await Promise.all(promises).then(restoreFrecencyTrigger)
           }
         }
       }
